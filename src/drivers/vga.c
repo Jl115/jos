b9598f47a42827cde640bc42e988ec7d049c83c2
jl115 <joelbern006@gmail.com>
2026-05-20 06:27:06 +0200

    fix: added formated and linter config

--- src/drivers/vga.c
+++ src/drivers/vga.c
@@ -1,38 +1,38 @@
 #include "vga.h"
 
-static const int VGA_WIDTH = 80;
-static const int VGA_HEIGHT = 25;
+static const int       VGA_WIDTH  = 80;
+static const int       VGA_HEIGHT = 25;
 static uint16_t *const VGA_MEMORY = (uint16_t *)0xB8000;
 
-static int terminal_row = 0;
+static int terminal_row    = 0;
 static int terminal_column = 0;
 
 void vga_clear(void) {
-  for (int y = 0; y < VGA_HEIGHT; y++) {
-    for (int x = 0; x < VGA_WIDTH; x++) {
-      // Leerzeichen ( ) in der Farbe Weiß (0x0F)
-      VGA_MEMORY[y * VGA_WIDTH + x] = (uint16_t)' ' | (uint16_t)0x0F << 8;
+    for (int y = 0; y < VGA_HEIGHT; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++) {
+            // Leerzeichen ( ) in der Farbe Weiß (0x0F)
+            VGA_MEMORY[y * VGA_WIDTH + x] = (uint16_t)' ' | (uint16_t)0x0F << 8;
+        }
     }
-  }
-  terminal_row = 0;
-  terminal_column = 0;
+    terminal_row    = 0;
+    terminal_column = 0;
 }
 
 void vga_print(const char *str, uint8_t color) {
-  for (int i = 0; str[i] != '\0'; i++) {
-    if (str[i] == '\n') {
-      terminal_column = 0;
-      terminal_row++;
-      continue;
-    }
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '\n') {
+            terminal_column = 0;
+            terminal_row++;
+            continue;
+        }
 
-    VGA_MEMORY[terminal_row * VGA_WIDTH + terminal_column] =
-        (uint16_t)str[i] | (uint16_t)color << 8;
-    terminal_column++;
+        VGA_MEMORY[terminal_row * VGA_WIDTH + terminal_column] = (uint16_t)str[i] | (uint16_t)color
+                                                                                        << 8;
+        terminal_column++;
 
-    if (terminal_column >= VGA_WIDTH) {
-      terminal_column = 0;
-      terminal_row++;
+        if (terminal_column >= VGA_WIDTH) {
+            terminal_column = 0;
+            terminal_row++;
+        }
     }
-  }
 }
--- src/kernel.c
+++ src/kernel.c
@@ -1,13 +1,14 @@
 #include "drivers/vga.h"
 
 void kernel_main(void) {
-  vga_clear();
+    vga_clear();
+    int i = 5;
 
-  // 0x0A = Hellgrün, 0x0F = White, 0x0C = Red
-  vga_print("JOS Modular Architecture: ONLINE\n", 0x0A);
-  vga_print("Module 1: VGA Text Driver Loaded.\n", 0x0F);
-  vga_print("System Ready.\n", 0x0C);
+    // 0x0A = Hellgrün, 0x0F = White, 0x0C = Red
+    vga_print("JOS Modular Architecture: ONLINE\n", 0x0A);
+    vga_print("Module 1: VGA Text Driver Loaded.\n", 0x0F);
+    vga_print("System Ready.\n", 0x0C);
 
-  while (1) {
-  }
+    while (1) {
+    }
 }
